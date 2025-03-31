28020b1dcba68c320139268f76cd04c565d2840d
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2025-03-31 18:16:34 -0400

    arrays

--- script.js
+++ script.js
@@ -19,11 +19,28 @@ for (let i = 1; i <= 15; ++i) {
 }
 
 let people = [18, 20, 16, 15, 21];
+let adults = [];
+
+/*
+let search = people.filter(element => {
+    console.log(element);
+    if (element < 18) {
+        console.log("sorry " + element);
+    } else {
+
+    adults.push(element);
+    }
+});
+console.log(adults);
+*/
+
 
 for (let i  = 0; i < people.length; i++) {
     if (people[i] < 18) {
         console.log("sorry "+ people[i] + " not allowed");
     } else {
         console.log("welcome "+ people[i]+ " come on in");
+        adults.push(people[i]);
     }
+    console.log(adults);
 }
