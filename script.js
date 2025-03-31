9bafdec4fd0dfe550b65c75746a62c10b7219def
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2025-03-31 18:09:34 -0400

    loop exercise

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ let hasMembership = true;
 let user = loggedIn || hasMembership ? "Show Video" : "Don't show video";
 
 console.log(user);
+
+for (let i = 1; i <= 15; ++i) {
+    
+    if (i % 5 === 0) {
+        console.log(`${i} - ASAP Frontend`);
+    } else if (i % 2 ==! 0) {
+        console.log(`${i} - ASAP`);
+    } else {
+        console.log(`${i} - Frontend`);
+    }
+}
+
+let people = [18, 20, 16, 15, 21];
+
+for (let i  = 0; i < people.length; i++) {
+    if (people[i] < 18) {
+        console.log("sorry "+ people[i] + " not allowed");
+    } else {
+        console.log("welcome "+ people[i]+ " come on in");
+    }
+}
